bdccb0e88478e4abf9dff3703867eae2f09e6c1b
_shAdoww <68319501+TECH-70@users.noreply.github.com>
2021-12-11 10:36:56 +0000

    Monotone hehe

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Monotone
+# Project Link
+
+https://linkedin-clone-2dc4d.web.app/
+
+## Build from Sonny Sangha YouTube Tutorial LinkedIn Clone
---
+++ src/Sidebar.js
@@ -0,0 +1,45 @@
+import { Avatar } from "@material-ui/core";
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
+import "./Sidebar.css";
+
+const Sidebar = () => {
+  const { user } = useSelector(selectUser);
+
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p>{topic}</p>
+    </div>
+  );
+
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <Avatar src={user.photoUrl} className="sidebar__avatar">
+          {user.email[0].toUpperCase()}
+        </Avatar>
+        <h2>{user.displayName}</h2>
+      </div>
+
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Friends</p>
+          <p className="sidebar__statNumber">2,300</p>
+        </div>
+        <div className="sidebar__stat">
+          <p>Likes on posts</p>
+          <p className="sidebar__statNumber">2,570</p>
+        </div>
+      </div>
+
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+        <h1>Screen time function</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
