bb0bb3ff58e789c877ec686fafd281e7626d3276
PiDPyd Dev <80102976+PiDPyd@users.noreply.github.com>
2022-01-29 19:39:46 +0530

    More tiny changes

--- components/Login.js
+++ components/Login.js
@@ -5,6 +5,7 @@ function Login({ providers }) {
   return (
     <div className="flex flex-col items-center space-y-20 pt-48">
       <h1 className="text-white text-5xl mb-10">Monotone</h1>
+      <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet"></link>
       <Image
           src="https://i.ibb.co/bWKZV3d/Screenshot-2022-01-24-180805.png"
           width={150}
@@ -16,7 +17,7 @@ function Login({ providers }) {
         {Object.values(providers).map((provider) => (
           <div key={provider.name}>
             <button
-              className="mt-[200px] relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-black rounded hover:bg-[#1e1e1e] group"
+              className="mt-[100px] relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-black rounded hover:bg-[#1e1e1e] group"
               onClick={() => signIn(provider.id, { callbackUrl: "/" })}
             >
               
