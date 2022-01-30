a26c676fc875355ca656f6190a3b1ccccc01510d
_shAdoww <68319501+TECH-70@users.noreply.github.com>
2022-01-30 04:46:35 +0000

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,6 +11,14 @@ export default NextAuth({
     // ...add more providers here
   ],
   callbacks: {
+    async jwt({ token, account }) {
+      // Persist the OAuth access_token to the token right after signin
+      if (account) {
+        token.accessToken = account.access_token
+      }
+      return token
+    },
+  },
     async session({ session, token }) {
       session.user.tag = session.user.name
         .split(" ")
